efcd863b2bb2dcedf9b30f67917070d8e65e2649
Roof Panichsombat <roofimon@gmail.com>
2013-04-01 23:37:50 +0700

    Reinitial Project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestSystem::Application.config.secret_token = 'a546826a7c697bf2a909fa9e83dc46dcdb0a65f27f81346ed510fbcfa8ad2430cecaf2a3e87e0bec14f264484ede1a7750223b533182df0625c6d41137d2cddf'
